d421a7ddb974b1169217f8a095bd89c5faefa5d1
tombelv <tommaso.belv@gmail.com>
2021-05-31 16:26:19 +0200

    corretta g

--- g.m
+++ g.m
@@ -1,5 +1,5 @@
 function g_sym = g(in1)
-    global state_coeff s_init s_fin ns
+    global state_coeff s_init ns
     [R, ~] = single_shooting(in1, s_init);
-    g_sym = sqrt(state_coeff) \ R(end-ns+1:end) - s_fin;
+    g_sym = sqrt(state_coeff) \ R(end-ns+1:end);
 end
